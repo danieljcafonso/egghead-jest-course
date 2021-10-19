59bf66ab390c78df48afe45308fcfce7e10a1be5
danieljcafonso <danieljcafonso@gmail.com>
2021-10-19 19:00:27 +0100

    feat(04): use mocks

--- index.test.js
+++ index.test.js
@@ -1,8 +1,10 @@
 const sum = require("./index");
+jest.mock("./index");
 
 describe("sum", () => {
   test("the sum of 2 + 1 should return 3", () => {
     // arrange
+    sum.mockImplementation(() => 3);
     const a = 2;
     const b = 1;
     const expectedResult = 3;
@@ -10,5 +12,17 @@ describe("sum", () => {
     const result = sum(a, b);
     // assert
     expect(result).toBe(expectedResult);
+    console.log(sum);
+    expect(sum).toHaveBeenCalledTimes(1);
+    expect(sum).toHaveBeenCalledWith(a, b);
+  });
+
+  test("expect callback to be called", () => {
+    const fn = (cb) => cb("text");
+    const mockedCallback = jest.fn();
+    fn(mockedCallback);
+
+    expect(mockedCallback).toHaveBeenCalledTimes(1);
+    expect(mockedCallback).toHaveBeenCalledWith("text");
   });
 });
